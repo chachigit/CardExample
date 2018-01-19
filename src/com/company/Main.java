ca6a273532f43af031acf67fa953afc1b75fa991
chachigit <seechachrun@hotmail.com>
2018-01-19 12:06:46 -0500

    added comments to main.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static void main(String[] args) {
         Deck deck = new PinochleDeck();
 
+        // pinochle has 48 cards. Looping to 54 to demonstrate that dealOneCard returns null if there are no more cards in the deck.
         for(int i = 1; i < 54; i++){
             ICard card = deck.dealOneCard();
             System.out.println(card != null ? card.name() : "No more cards left in deck.");
@@ -20,6 +21,7 @@ public class Main {
 
         deck = new StandardDeck();
         deck.shuffle();
+        // iterate to deck.getLength => will not get a null from the deck.
         for(int i = 1; i < deck.getLength(); i++){
             ICard card = deck.dealOneCard();
             deck.shuffle();
